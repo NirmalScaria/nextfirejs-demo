4df0c3ba2072ba68ba4cb1f727d714ac30bf8de6
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-06-23 15:50:23 +0530

    [INIT] NextFireJS Setup

---
+++ firebase-app-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "nextfire-test-29ebd",
+  "appId": "1:279146484075:web:237cc6ed7a68b15fbbbc2e",
+  "storageBucket": "nextfire-test-29ebd.appspot.com",
+  "apiKey": "AIzaSyAnGjxsa62dhtVLaJXa-PzHl8oi7BiI5E0",
+  "authDomain": "nextfire-test-29ebd.firebaseapp.com",
+  "messagingSenderId": "279146484075"
+}
